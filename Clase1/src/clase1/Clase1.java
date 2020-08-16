7b15c7314b16bab8b0acb8fb3702975e61ba9b11
oscar2020-jpg <69723477+oscar2020-jpg@users.noreply.github.com>
2020-08-16 03:05:33 +0200

    HOLA MUNDO
    SE AGREGO UN HOLA MUNDO

--- Clase1/src/clase1/Clase1.java
+++ Clase1/src/clase1/Clase1.java
@@ -15,7 +15,8 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODSO code application logic here
+        System.out.println("clase1.Clase1.main(HOLA GITHUBS)");
     }
     
 }
